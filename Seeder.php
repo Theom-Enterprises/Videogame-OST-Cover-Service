7b46920b3381f8689ad89505cfe10c1111187aed
Alexander Sarka <alexander.sarka@htl.rennweg.at>
2021-12-07 10:24:56 +0100

    Update index.php

--- Seeder.php
+++ Seeder.php
@@ -10,20 +10,20 @@ class Seeder
 
     public static function seed(): array
     {
-        $aloneInTheDarkSongs[] = new Song(1, 'Prelude to an End', 'Olivier Deriviere', 1, 60+33);
-        $aloneInTheDarkSongs[] = new Song(1, 'Edward Carnby', 'Olivier Deriviere', 2, 2*60+36);
-        $aloneInTheDarkSongs[] = new Song(1, 'The Fissure', 'Olivier Deriviere', 3, 3*60+43);
-        $aloneInTheDarkSongs[] = new Song(1, 'Collapsing Floors', 'Olivier Deriviere', 4, 4*60+35);
+        $aloneInTheDarkSongs[] = new Song(11, 'Prelude to an End', 'Olivier Deriviere', 1, 60+33);
+        $aloneInTheDarkSongs[] = new Song(12, 'Edward Carnby', 'Olivier Deriviere', 2, 2*60+36);
+        $aloneInTheDarkSongs[] = new Song(13, 'The Fissure', 'Olivier Deriviere', 3, 3*60+43);
+        $aloneInTheDarkSongs[] = new Song(14, 'Collapsing Floors', 'Olivier Deriviere', 4, 4*60+35);
 
-        $starControlOriginsSongs[] = new Song(1, 'Boundaries and Bylaws', 'Mason Fisher', 1, 3*60+8);
-        $starControlOriginsSongs[] = new Song(1, 'Take to the Stars', 'Riku Nuottajärvi', 2, 5*60+46);
-        $starControlOriginsSongs[] = new Song(1, 'A Higher Power', 'Riku Nuottajärvi', 3, 2*60+57);
-        $starControlOriginsSongs[] = new Song(1, 'Rich Vein', 'Riku Nuottajärvi', 4, 60+47);
+        $starControlOriginsSongs[] = new Song(21, 'Boundaries and Bylaws', 'Mason Fisher', 1, 3*60+8);
+        $starControlOriginsSongs[] = new Song(22, 'Take to the Stars', 'Riku Nuottajärvi', 2, 5*60+46);
+        $starControlOriginsSongs[] = new Song(23, 'A Higher Power', 'Riku Nuottajärvi', 3, 2*60+57);
+        $starControlOriginsSongs[] = new Song(24, 'Rich Vein', 'Riku Nuottajärvi', 4, 60+47);
 
-        $needForSpeedSongs[] = new Song(1, 'The Crew', 'Photek', 1, 60+35);
-        $needForSpeedSongs[] = new Song(1, 'Pursuit Low', 'Photek', 2, 3*60+18);
-        $needForSpeedSongs[] = new Song(1, 'Pursuit Hi', 'Photek', 3, 4*60+43);
-        $needForSpeedSongs[] = new Song(1, 'Cool Down', 'Photek', 4, 60+47);
+        $needForSpeedSongs[] = new Song(31, 'The Crew', 'Photek', 1, 60+35);
+        $needForSpeedSongs[] = new Song(32, 'Pursuit Low', 'Photek', 2, 3*60+18);
+        $needForSpeedSongs[] = new Song(33, 'Pursuit Hi', 'Photek', 3, 4*60+43);
+        $needForSpeedSongs[] = new Song(34, 'Cool Down', 'Photek', 4, 60+47);
 
         return [
             new Soundtrack(1, 'Alone in the Dark (Original Soundtrack from the Video Game)', 'Alone in the Dark', 2008, $aloneInTheDarkSongs),
--- index.php
+++ index.php
@@ -7,10 +7,10 @@ require_once 'Seeder.php';
 header("content-type:application/json");
 
 if (isset($_GET['ost'])) {
-    $soundtracks[] = Seeder::seed();
+    $soundtracks = Seeder::seed();
     if ($_GET['ost'] == 'all') {
         echo json_encode($soundtracks);
-    } else if (in_array($_GET['ost'], $soundtracks['uid'])) {
+    } else if (in_array($_GET['ost'], $soundtracks)) {
         echo json_encode($soundtracks[$_GET['ost']]);
     } else {
         echo json_encode(['Fehler' => "Dieses OST wurde nicht gefunden: {$_GET['ost']}"]);
