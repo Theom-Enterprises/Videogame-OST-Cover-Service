fa7d94905c3b4774097530c908067dcb6b5114d4
Omar Faid <8132@htl.rennweg.at>
2021-12-14 09:03:05 +0100

    GET Parameter sanitization

--- index.php
+++ index.php
@@ -6,13 +6,16 @@ require_once 'Seeder.php';
 
 header("content-type:application/json");
 
+
+
 if (isset($_GET['ost'])) {
     $soundtracks = Seeder::seed();
+    $ost = preg_replace('/\\D/', '', $_GET['ost']);
     if ($_GET['ost'] == 'all') {
         echo json_encode($soundtracks);
-    } else if (array_key_exists($_GET['ost']-1, $soundtracks)) {
-        echo json_encode($soundtracks[$_GET['ost']-1]);
+    } else if (array_key_exists($ost-1, $soundtracks)) {
+        echo json_encode($soundtracks[$ost-1]);
     } else {
-        echo json_encode(['Fehler' => "Dieses OST wurde nicht gefunden: {$_GET['ost']}"]);
+        echo json_encode(['Fehler' => "Dieses OST wurde nicht gefunden: {$ost}"]);
     }
 }
