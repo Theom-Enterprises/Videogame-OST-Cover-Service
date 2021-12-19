fcf17a315a83594762d47797f6d20386edbedbc7
Daniel Lozada <daniellozada1702@gmail.com>
2021-12-19 13:46:33 +0100

    Form hinzugefügt .htaccess erstellt für SEO freundliche URL Songs & Soundtracks werden nach UID gesucht

--- index.php
+++ index.php
@@ -8,12 +8,50 @@ header("content-type:application/json");
 
 if (isset($_GET['ost'])) {
     $soundtracks = Seeder::seed();
-    $ost = preg_replace('/\\D/', '', $_GET['ost']);
-    if ($_GET['ost'] == 'all') {
+    $ost = preg_replace('/\D/', '', $_GET['ost']);
+    if ($_GET['ost'] === 'all') {
         echo json_encode($soundtracks);
-    } else if (array_key_exists($ost - 1, $soundtracks)) {
-        echo json_encode($soundtracks[$ost - 1]);
+    } else if (findSoundtrackByID($soundtracks, $ost)) {
+        $soundtrack = findSoundtrackByID($soundtracks, $ost);
+        if (isset($_GET['song'])) {
+            $song = preg_replace('/\D/', '', $_GET['song']);
+            $songs = $soundtrack->getTrackList();
+            if (findSongByID($songs, $song)) {
+                echo json_encode(findSongByID($songs, $song));
+            } else {
+                echo json_encode(['Fehler' => "Dieser Song wurde nicht gefunden: $song"]);
+            }
+        } else {
+            echo json_encode($soundtrack);
+        }
     } else {
-        echo json_encode(['Fehler' => "Dieses OST wurde nicht gefunden: {$ost}"]);
+        echo json_encode(['Fehler' => "Dieses OST wurde nicht gefunden: $ost", "Test" => findSoundtrackByID($soundtracks, $ost)]);
     }
 }
+
+/**
+ * @param Soundtrack[] $soundtracks
+ */
+function findSoundtrackByID(array $soundtracks, $id)
+{
+    foreach ($soundtracks as $soundtrack) {
+        if ((int)$id === $soundtrack->getUid()) {
+            return $soundtrack;
+        }
+    }
+    return false;
+}
+
+/**
+ * @param Song[] $songs
+ */
+function findSongByID(array $songs, $id)
+{
+    foreach ($songs as $song) {
+        if ((int)$id === $song->getUid()) {
+            return $song;
+        }
+    }
+    return false;
+}
+
